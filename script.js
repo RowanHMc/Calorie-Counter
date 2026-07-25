7ce322cec273724671bfe7b96b0cb7dc4e6977fc
Rowan <rowanhadegu@gmail.com>
2026-07-25 18:29:58 +0300

    slect the elements for js to access in HTMLusing its ID

---
+++ script.js
@@ -0,0 +1,6 @@
+const foodForm = document.getElementById("FoodForm");
+const foodName = document.getElementById("FoodName");
+const foodCalories = document.getElementById("FoodCalories");
+const foodList = document.getElementById("FoodList");
+const totalCalories = document.getElementById("TotalCalories");
+const resetBtn = document.getElementById("ResetBtn");
